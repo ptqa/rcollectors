f01d7e34a28be89c09bb187e58419f46caeea1a4
ptQa <ptqa.mail@gmail.com>
2015-03-19 23:19:07 +0300

    return nothing on failure

--- redis.rb
+++ redis.rb
@@ -12,18 +12,13 @@ def main()
   redis_host = config[:host] || '127.0.0.1'
   redis_port = config[:port] || '6379'
   redis_conn = Redis.new(host: redis_host, port: redis_port)
-  begin
-    config[:metrics].each do |metric|
-      taglist = ''
-      metric[:tags].each do |tag,value|
-        taglist += "#{tag}=#{value} "
-      end if metric[:tags]
-      puts "redis.#{metric[:name]}  #{Time.now.to_i} #{redis_conn.send(metric[:cmd],metric[:args])} #{taglist}"
-    end
-  rescue Redis::BaseConnectionError => error
-    puts "#{error}, retrying in 1s"
-    sleep 1
-    retry
+  config[:metrics].each do |metric|
+    metric_value = redis_conn.send(metric[:cmd],metric[:args])
+    taglist = ''
+    metric[:tags].each do |tag,value|
+      taglist += "#{tag}=#{value} "
+    end if metric[:tags]
+    puts "redis.#{metric[:name]}  #{Time.now.to_i} #{metric_value} #{taglist}" if metric_value
   end
 end
 
